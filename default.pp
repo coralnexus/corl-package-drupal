48251336e709914975c6df3ca0e4d37b95ba2937
Adrian Webb <adrian.webb@coraltech.net>
2015-02-10 09:38:53 -0500

    Removing the php module class defaults.

--- default.pp
+++ default.pp
@@ -1,13 +1,5 @@
 
 class coralnexus::drupal::default {
 
-  $apache_drupal_classes = [
-    "php::mod::apc",
-    "php::mod::curl",
-    "php::mod::gd",
-    "php::mod::xmlrpc",
-    "php::mod::uploadprogress",
-    "php::mod::xdebug"
-  ]
 }
 
